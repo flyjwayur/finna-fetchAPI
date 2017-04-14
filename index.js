e86a3fb720253f13f7f8af4b78f871c35e23a3c0
Hyesoo Park <flyjwayur@gmail.com>
2017-04-14 09:41:14 +0300

    Fix img.src address

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /**
  * Created by soo on 12.4.2017.
  */
+
+const ul = document.getElementById('finna');
+const url = 'https://api.finna.fi/v1/search?lookfor=sibelius&filter[]=online_boolean:%221%22&filter[]=format:%220/Image/%22';
+
 function createNode(element) {
     return document.createElement(element);
 }
@@ -9,18 +13,17 @@ function append(parent, el) {
     return parent.appendChild(el);
 }
 
-const ul = document.getElementById('finna');
-const url = 'https://api.finna.fi/v1/search?lookfor=sibelius&filter[]=online_boolean:%221%22&filter[]=format:%220/Image/%22';
 fetch(url)
     .then((resp) => resp.json())
     .then(function(data) {
         let architectures = data.records;
+        //console.log(JSON.stringify(data.records, null, 2));
         return architectures.map(function(building) {
-            console.log(JSON.stringify(architectures));
+            console.log(JSON.stringify(architectures, null, 2));
             let li = createNode('li'),
                 img = createNode('img');
                 //span = createNode('span');
-            img.src = "https://finna.fi/Cover/Show?id="+building.id;
+            img.src = "http://api.finna.fi"+building.images[0];
             console.log(img.src);
 
             //span.innerHTML = `${author.name.first} ${author.name.last}`;
