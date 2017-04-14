d4045ae65b3741cbde9abdd584e5c4752a7efc5e
Hyesoo Park <flyjwayur@gmail.com>
2017-04-14 09:43:49 +0300

    Need to fix : pixi.js

--- index.js
+++ index.js
@@ -2,6 +2,41 @@
  * Created by soo on 12.4.2017.
  */
 
+//render images with pixi.js
+var loader_ = new PIXI.loaders.Loader();
+var stage_ = new PIXI.Container();
+var renderer_ = PIXI.autoDetectRenderer(256, 256);
+document.body.appendChild(renderer_.view);
+
+var testURL = 'http://api.finna.fi/Cover/Show?id=musketti.M012%3AHK6869%3A1.2&index=0&size=large';
+
+//With Pixi.js : it works with 'player.png' but it does not work with api.finna.fi url
+loader_
+    .add([{url: testURL,
+        crossOrigin: true}])
+    .on("progress", loadProgressHandler)
+    .load(setup);
+
+function loadProgressHandler(loader, resource) {
+
+    //Display the file `url` currently being loaded
+    console.log("loading: " + resource.url);
+
+    //Display the precentage of files currently loaded
+    console.log("progress: " + loader.progress + "%");
+
+    //If you gave your files names as the first argument
+    //of the `add` method, you can access them like this
+    //console.log("loading: " + resource.name);
+}
+
+function setup(){
+    var sprite = new PIXI.Sprite( loader_.resources[testURL].texture);
+
+    stage_.addChild(sprite);
+    renderer_.render(stage_);
+}
+
 const ul = document.getElementById('finna');
 const url = 'https://api.finna.fi/v1/search?lookfor=sibelius&filter[]=online_boolean:%221%22&filter[]=format:%220/Image/%22';
 
@@ -30,7 +65,7 @@ fetch(url)
             append(li, img);
             //append(li, span);
             append(ul, li);
-        })
+        });
     })
     .catch(function(error) {
         console.log(JSON.stringify(error));
