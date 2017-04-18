9298fd4059271c1e81f848a3b4ae6b7b655c53be
Hyesoo Park <flyjwayur@gmail.com>
2017-04-18 23:35:43 +0300

    need to fix: returning function

--- index.js
+++ index.js
@@ -10,14 +10,22 @@ document.getElementById("pixi").appendChild(renderer_.view);
 var testURL = "https://api.finna.fi/Cover/Show?id=muusa.urn%3Auuid%3A7682B120-4F8E-4210-AD4D-1B118BA7699E&index=0&size=large";
 
 let base_image = new Image();
-base_image.addEventListener("load", imageOnLoad);
+base_image.addEventListener("load", passBaseImage);
 base_image.src = testURL;
 
-function imageOnLoad(event) {
-  console.log(event.target);
-  var base = new PIXI.BaseTexture(base_image),
-    texture = new PIXI.Texture(base),
-    sprite = new PIXI.Sprite(texture);
-  stage_.addChild(sprite);
-  renderer_.render(stage_);
+function passBaseImage (base_image) {
+    return function imageOnLoad(event) {
+        console.log(event.target);
+        console.log("base_image width & height "+ base_image.width, base_image.height);
+
+        var rectangle = new PIXI.Rectangle(100,100,100,100);
+        //Tell the texture to use that rectangular section
+        var base = new PIXI.BaseTexture(base_image),
+            texture = new PIXI.Texture(base);
+        texture.frame = rectangle;
+        var sprite = new PIXI.Sprite(texture);
+        stage_.addChild(sprite);
+        renderer_.render(stage_);
+    }
 }
+
